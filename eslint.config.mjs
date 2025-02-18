efa8e4231095d25e7093284be058372deca94991
leon <lza.bupt@gmail.com>
2025-02-18 09:57:11 +0800

    init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import { includeIgnoreFile } from '@eslint/compat';
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+const gitIgnorePath = resolve(__dirname, '.gitignore');
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  includeIgnoreFile(gitIgnorePath),
+  {
+    ignores: [
+      '**/coverage/',
+      '**/public/',
+      '**/dist/',
+      'pnpm-lock.yaml',
+      'pnpm-workpace.yaml',
+    ],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  pluginPrettierRecommended,
 ];
 
 export default eslintConfig;
