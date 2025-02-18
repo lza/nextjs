78e647b6c64ce0ca3a9b54055c36ff2a824aa063
leon <lza.bupt@gmail.com>
2025-02-18 16:50:39 +0800

    reset

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="min-100dvh mx-auto mt-2 max-w-7xl rounded border bg-pink-500 p-2 text-center text-3xl font-bold text-indigo-400 underline ring-2 shadow-2xl">
-        Hello World!
-      </h1>
-    </>
-  );
-}
